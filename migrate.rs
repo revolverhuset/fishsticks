e67b33f705a13b002ee39d5e65195d62a5f82340
Magnus Hoff <maghoff@gmail.com>
2016-11-20 11:32:17 +0100

    Cleanup migration.rs

--- migrate.rs
+++ migrate.rs
@@ -1,13 +1,11 @@
-#![feature(proc_macro)]
-#[macro_use] extern crate diesel;
+extern crate diesel;
 
 use diesel::Connection;
-use diesel::sqlite::SqliteConnection;
 
 fn main() {
     let _ = std::fs::remove_file(".build.db");
 
-    let connection = SqliteConnection::establish(".build.db")
+    let connection = diesel::sqlite::SqliteConnection::establish(".build.db")
         .expect(&format!("Error esablishing a database connection to .build.db"));
 
     diesel::migrations::run_pending_migrations(&connection).unwrap();
