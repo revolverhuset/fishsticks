9062baa0600f4f84cd4f081cfd168248974af122
Magnus Hoff <maghoff@gmail.com>
2016-11-25 10:03:05 +0100

    Make searching case insensitive

---
+++ migrations/20161125081650_change_columns_to_collate_nocase/down.sql
@@ -0,0 +1,30 @@
+PRAGMA foreign_keys=OFF;
+
+CREATE TABLE new_restaurants (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO new_restaurants SELECT * FROM restaurants;
+
+DROP TABLE restaurants;
+ALTER TABLE new_restaurants RENAME TO restaurants;
+
+CREATE TABLE new_menu_items (
+    id INTEGER PRIMARY KEY NOT NULL,
+    restaurant INTEGER NOT NULL,
+    'number' INTEGER NOT NULL,
+    name TEXT NOT NULL COLLATE NOCASE,
+    price_in_cents INTEGER NOT NULL,
+    FOREIGN KEY(restaurant) REFERENCES restaurants(id),
+    UNIQUE (restaurant, 'number')
+);
+
+INSERT INTO new_menu_items SELECT * FROM menu_items;
+
+DROP TABLE menu_items;
+ALTER TABLE new_menu_items RENAME TO menu_items;
+
+PRAGMA foreign_key_check;
+
+PRAGMA foreign_keys=ON;
